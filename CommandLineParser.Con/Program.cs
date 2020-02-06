3f4e5925f2378eae5c510d13e5ffc3e3befb7101
David Ruiz <druizg@gmail.com>
2020-02-06 20:26:33 +0100

    comnnadn line parser

---
+++ CommandLineParser.Con/Program.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------
+// <copyright>Copyright (C) 2020, David Ruiz.</copyright>
+// Licensed under the Apache License, Version 2.0.
+// You may not use this file except in compliance with the License:
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Software is distributed on an "AS IS", WITHOUT WARRANTIES
+// OR CONDITIONS OF ANY KIND, either express or implied.
+// -----------------------------------------------------------------
+
+namespace CommandLineParser.Con
+{
+    using System;
+    using CommandLine;
+
+    public class Options
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+        public bool Verbose { get; set; }
+
+        public static Options Default()
+        {
+            return new Options
+            {
+                Verbose = true
+            };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CommandLineParser DEMO!");
+
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (o.Verbose)
+                    {
+                        Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                        Console.WriteLine("Quick Start Example!");
+                    }
+                });
+        }
+    }
+}
---
+++ dotnet-script.cmd
@@ -0,0 +1,46 @@
+:: ------------------------------------------------------------------------------------------------------------------
+:: Copyright (C) 2020, David Ruiz. Licensed under the Apache License, Version 2.0. 
+:: You may not use this file except in compliance with the License: http://www.apache.org/licenses/LICENSE-2.0
+:: Software is distributed on an "AS IS", WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+:: ------------------------------------------------------------------------------------------------------------------
+
+SET PROJECTNAME=AdventureWorks
+SET REPOSITORY=https://github.com/<account>/%PROJECTNAME%.git
+
+git clone %REPOSITORY% %PROJECTNAME%
+cd                     %PROJECTNAME%
+
+git init
+dotnet new gitignore
+git add .
+git commit -m "startup project"
+git push
+
+git checkout -b dev
+git push --set-upstream origin dev
+
+dotnet new sln         %PROJECTNAME%
+
+dotnet new classlib -n %PROJECTNAME%.App -f netcoreapp3.1
+
+dotnet new classlib -n %PROJECTNAME%.Data -f netcoreapp3.1
+dotnet sln add         %PROJECTNAME%.Data
+
+dotnet new classlib -n %PROJECTNAME%.Dom 
+dotnet sln add         %PROJECTNAME%.Dom
+dotnet new mstest   -n %PROJECTNAME%.Tests
+dotnet sln add         %PROJECTNAME%.Tests
+
+dotnet new console  -n %PROJECTNAME%.Con
+dotnet sln add         %PROJECTNAME%.Con
+
+
+:: dotnet add package Microsoft.EntityFrameworkCore.Tools 
+:: dotnet add package Microsoft.EntityFrameworkCore.Design
+:: dotnet add package Microsoft.EntityFrameworkCore.InMemory
+:: dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+:: dotnet add package System.ComponentModel.Annotations
+
+:: Code Analysis
+:: dotnet add package Microsoft.CodeAnalysis.FxCopAnalyzers
+:: dotnet add package SecurityCodeScan
