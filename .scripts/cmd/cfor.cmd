50ef494fa5f04206470a7fb159d8189f6a541ff2
David Ruiz <druizg@gmail.com>
2020-04-12 11:11:40 +0200

    for example

--- .scripts/cmd/cfor.cmd
+++ .scripts/cmd/cfor.cmd
@@ -1,18 +1,12 @@
-::@echo off
+@echo off
 set rootpath=%1
-set currentpath=%cd%
 
 if "%rootpath%"=="" set rootpath=.
-::cd %rootpath%\
+
 @setlocal enableextensions enabledelayedexpansion
-for /f "delims=" %%p in ('dir /b %rootpath%\*.txt') do (
+
+for /f "delims=" %%p in ('dir /b /s %rootpath%\*.csproj') do (
 	set x=%%p
-	echo file: !x:~0,-4!
+	echo file: !x:~0,-7!
 )
-
-for /f %%p in ('dir /b *.dll') do (
-	echo -^> file: %%p	
-)
-pause
-
-::cd /D %currentpath%
+::for /f %%p in ('dir /b /s %rootpath%\*.csproj') do (	echo -^> file: %%p	)
